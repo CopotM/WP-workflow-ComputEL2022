7e955be3676ee3143eca0bf3f26cf8288e8ceb79
elsner.14 <elsner.14@u084.unity>
2021-11-04 11:52:52 -0400

    wao multisyll baseline run

--- tokenize_wao.py
+++ tokenize_wao.py
@@ -7,9 +7,14 @@ if __name__ == "__main__":
     dd = sys.argv[1]
     out = sys.argv[2]
 
+    if dd.endswith(".txt"):
+        path = dd
+    else:
+        path = dd + "/*.txt"
+
     with open(out, "w") as ofh:
 
-        for fname in glob.glob(dd + "/*.txt"):
+        for fname in glob.glob(path):
             print(fname)
             with open(fname) as fh:
                 for line in fh:
